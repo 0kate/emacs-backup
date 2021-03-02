a9301b216ee70f416436efa3be3912c74b47d85f
0K4T3 <32959438+0K4T3@users.noreply.github.com>
2021-03-02 19:21:14 +0900

    Update init.el

--- init.el
+++ init.el
@@ -1,26 +1,14 @@
 (require 'package)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(exec-path-from-shell-initialize)
-
-;; eshell
-(defun m-eshell-hook ()
-  (define-key eshell-mode-map [(control p)] 'eshell-previous-matching-input-from-input)
-  (define-key eshell-mode-map [(control n)] 'eshell-next-matching-input-from-input)
- 
-  (define-key eshell-mode-map [up] 'previous-line)
-  (define-key eshell-mode-map [down] 'next-line))
-(add-hook 'eshell-mode-hook 'm-eshell-hook)
-
-;; shell
-(defun m-shell-hook ()
-  (define-key shell-mode-map [(control p)] 'comint-previous-command)
-  (define-key shell-mode-map [(control n)] 'comint-next-command))
-
-;; theme
-(load-theme 'monokai t)
+;; initialize use-packge
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
 
 ;; linum
 (global-linum-mode 1)
@@ -33,23 +21,61 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+;; custom variable
+(setq custom-file (concat user-emacs-directory "/custom.el"))
+
+;; global keybinds
 (global-set-key (kbd "<f5>") 'eval-buffer)
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
-(use-package rbenv
+;; ivy
+(use-package ivy
+  :ensure t
   :config
-  (global-rbenv-mode)
-  (setq rbenv-installation-dir "~/.anyenv/envs/rbenv"))
+  (ivy-mode 1))
 
-;; ido
-(ido-mode 1)
-(ido-everywhere 1)
+;; ivy-ricy
+(use-package ivy-rich
+  :ensure t
+  :config
+  (ivy-rich-mode 1))
+
+;; counsel
+(use-package counsel
+  :ensure t
+  :config
+  (counsel-mode 1)
+  :bind
+  ("M-x" . counsel-M-x))
 
 ;; all-the-icons
-(use-package all-the-icons)
+(use-package all-the-icons
+  :ensure t)
+
+;; org
+(use-package org
+  :ensure t)
+
+;; avy
+(use-package avy
+  :ensure t
+  :bind
+  ("M-g l" . avy-goto-line)
+  ("M-g w" . avy-goto-word-1))
+
+;; magit
+(use-package magit
+  :ensure t)
+
+;; monokai-theme
+(use-package monokai-theme
+  :ensure t
+  :config
+  (load-theme 'monokai t))
 
 ;; neotree
 (use-package neotree
+  :ensure t
   :config
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
   :bind
@@ -58,35 +84,73 @@
 
 ;; centaur-tabs
 (use-package centaur-tabs
+  :ensure t
   :demand
   :config
   (centaur-tabs-mode t)
+  (centaur-tabs-headline-match)
+  (setq centaur-tabs-style "bar")
+  (setq centaur-tabs-set-bar 'left)
+  (setq centaur-tabs-set-icons t)
+  (setq centaur-tabs-gray-out-icons 'buffer)
+  (setq centaur-tabs-set-modified-marker t)
+  (setq centaur-tabs-modified-marker "◉")
   :bind
   ("<C-iso-lefttab>" . centaur-tabs-backward)
   ("C-<tab>" . centaur-tabs-forward))
 
-;; powerline
-(use-package powerline
-  :config
-  (powerline-default-theme))
-
 ;; dashboard
 (use-package dashboard
   :ensure t
+  :diminish
+  (dashboard-mode page-break-lines-mode)
   :config
   (dashboard-setup-startup-hook))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (monokai-theme spacemacs-theme rbenv dashboard use-package neotree markdown-preview-mode markdown-preview-eww markdown-mode+ magit exec-path-from-shell centaur-tabs all-the-icons))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; doom-themes
+(use-package doom-themes
+  :ensure t
+  :custom
+  (doom-themes-enable-italic t)
+  (doom-themes-enable-bold t)
+  :config
+  (load-theme 'doom-molokai t)
+  (doom-themes-org-config))
+
+;; doom-modeline
+(use-package doom-modeline
+  :ensure t
+  :custom
+  (doom-modeline-icon t)
+  :config
+  (doom-modeline-mode 1))
+
+;; lsp-mode
+(use-package lsp-mode
+  :ensure t)
+
+;; lsp-ui
+(use-package lsp-ui
+  :ensure t)
+
+;; indent-guide
+(use-package indent-guide
+  :ensure t
+  :config
+  (indent-guide-global-mode))
+
+;; rainbow-delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :config
+  (rainbow-delimiters-mode 1))
+
+;; paren
+(use-package paren
+  :ensure t
+  :config
+  (show-paren-mode 1))
+
+;; markdown-preview-mode
+(use-package markdown-preview-mode
+  :ensure t)
