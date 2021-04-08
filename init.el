51064f4c2e8cbd1e9e9dc25923352bb268bcd924
0K4T3 <o.keito317@icloud.com>
2021-04-08 22:12:02 +0900

    UPDATE: update company delay

--- init.el
+++ init.el
@@ -205,6 +205,7 @@
   :ensure t
   :config
   (global-company-mode t)
+  (setq company-idle-delay 0)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
